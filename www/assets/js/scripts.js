3cbec17869e4e8bc8ab986207ab5dd39afd0b352
Carson Bain <33740139+CarsonBain@users.noreply.github.com>
2019-02-26 11:22:16 -0800

    Updates for A360 issues

--- www/assets/js/scripts.js
+++ www/assets/js/scripts.js
@@ -97,7 +97,72 @@
 };
 
 //Photogallery Plugin and Equalize Plugin
-;(function($,undefined){$.fn.v65PhotoGallery=function(options){var defaults={galleryId : $("#pagePhotoGallery").attr("v65jsphotogalleryid"),galleryHeight : $("#pagePhotoGallery").attr("v65jsphotogalleryheight"),galleryWidth : $("#pagePhotoGallery").attr("v65jsphotogallerywidth"),timestamp : "×tamp="+ new Date().getTime(),effect:'fade',slices:15, animSpeed:500,pauseTime:5000, startSlide:0, directionNav:true,directionNavHide:true,controlNav:true},gallery=$(this),settings=$.extend(defaults, options);gallery.html("").css({"height":settings.galleryHeight,"width":settings.galleryWidth,"overflow":"hidden"});$.ajax({type: "GET",url: "/index.cfm?method=pages.showPhotoGalleryXML&photogalleryid="+settings.galleryId+defaults.timestamp,dataType: "xml",success: function(xml){var images="";$(xml).find('img').each(function(){var location='/assets/images/photogallery/images/large/',photo=$(this).attr('src'),caption=$(this).attr('caption'),url=$(this).attr('link');if (url===undefined){images +='<img src="'+location+photo+'" title="'+caption+'"/>';}else{images +='<a href="'+url+'"><img src="'+location+photo+'" title="'+caption+'"/></a>';}});gallery.append(images);},complete: function(){gallery.nivoSlider({effect:settings.effect,slices:settings.slices,animSpeed:settings.animSpeed,pauseTime:settings.pauseTime,startSlide:settings.startSlide,directionNav:settings.directionNav,directionNavHide:settings.directionNavHide,controlNav:settings.controlNav});}});};$.fn.equalize=function(length){for(var i=0;i < this.length;i+=length){var elems=this.slice(i, i+length),equalizeArray=[];for(j=0;j < length;j++){equalizeArray.push(elems.eq(j).height());}var height=Math.max.apply( Math, equalizeArray);elems.css('min-height', height);}return this;};})(jQuery);
+;(function($,undefined){
+	$.fn.v65PhotoGallery=function(options){
+		var defaults={
+			galleryId : $("#pagePhotoGallery").attr("v65jsphotogalleryid"),
+			galleryHeight : $("#pagePhotoGallery").attr("v65jsphotogalleryheight"),
+			galleryWidth : $("#pagePhotoGallery").attr("v65jsphotogallerywidth"),
+			timestamp : "×tamp="+ new Date().getTime(),
+			effect:'fade',
+			slices:15, 
+			animSpeed:500,
+			pauseTime:5000, 
+			startSlide:0, 
+			directionNav:true,
+			directionNavHide:true,
+			controlNav:true
+		},
+		gallery=$(this),settings=$.extend(defaults, options);
+		gallery.html("").css({
+			"height":settings.galleryHeight,
+			"width":settings.galleryWidth,
+			"overflow":"hidden"
+		});
+		$.ajax({
+			type: "GET",
+			url: "/index.cfm?method=pages.showPhotoGalleryXML&photogalleryid="+settings.galleryId+defaults.timestamp,
+			dataType: "xml",
+			success: function(xml){
+				var images="";
+				$(xml).find('img').each(function(){
+					var location='/assets/images/photogallery/images/large/',
+					photo=$(this).attr('src'),
+					caption=$(this).attr('caption'),
+					alt = $(this).attr('src'),
+					url=$(this).attr('link');
+					if (url===undefined){
+						images +='<img src="'+location+photo+'" alt="'+alt+'" title="'+caption+'"/>';
+					}else{
+						images +='<a href="'+url+'"><img src="'+location+photo+'" alt="'+alt+'" title="'+caption+'"/></a>';
+					}
+				});
+				gallery.append(images);
+			},
+			complete: function(){
+				gallery.nivoSlider({
+					effect:settings.effect,
+					slices:settings.slices,
+					animSpeed:settings.animSpeed,
+					pauseTime:settings.pauseTime,
+					startSlide:settings.startSlide,
+					directionNav:settings.directionNav,
+					directionNavHide:settings.directionNavHide,
+					controlNav:settings.controlNav
+				});
+			}
+		});
+	};
+	$.fn.equalize=function(length){
+		for(var i=0;i < this.length;i+=length){
+			var elems=this.slice(i, i+length),equalizeArray=[];
+			for(j=0;j < length;j++){
+				equalizeArray.push(elems.eq(j).height());
+			}var height=Math.max.apply( Math, equalizeArray);
+			elems.css('min-height', height);
+		}return this;
+	};
+})(jQuery);
 
 v65.global.init();
 v65.page.initPhotoGallery();
